47a1715f826faa2f54dd1f90f0ac8d28ebc8888c
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2022-03-02 19:19:51 -0300

    escrevemos o modulo de gcp instance

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "google_compute_instance" "this" {
+  count = var.amount
+
+  name         = format("%s-%d", var.name, count.index)
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags         = [terraform.workspaces]
+
+  metadata = {
+    ssh-keys = join("\n", [for key in var.ssh_keys : "${key.user}:${key.publickey}"])
+  }
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = var.network
+
+    access_config {
+      // Ephemerasl IP
+    }
+  }
+
+  metadata_startup_script = file(var.metadata_startup_script)
+}
---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "amount" {
+  description = "Quantidade de máquinas que deseja criar"
+  type        = number
+  default     = 1
+}
+
+variable "name" {
+  description = "Nome da instância"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "Tamanho da instância"
+  type        = string
+  default     = "f1-micro"
+}
+
+variable "zone" {
+  description = "Em qual zona sua instância irá ficar"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "image" {
+  description = "Qual Sistema deseja utilizar"
+  type        = string
+  default     = "debian-cloud/debian-9"
+}
+
+variable "network" {
+  description = "Qual rede deseja utilizar"
+  type        = string
+  default     = "default"
+}
+
+variable "metadata_startup_script" {
+  type        = string
+  description = "Caminho do script shell do userdata"
+  default     = null
+}
+
+variable "ssh_keys" {
+  type = list(object({
+    publickey = string
+    user      = string
+  }))
+  description = "Lista de PUB SSH KEY, que precisam ter acesso as VM"
+  default = [{
+    publickey = "ssh-rsa yourkeyabc username@PC"
+    user      = "username"
+  }]
+}
